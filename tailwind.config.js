7e356eefee9fce1af9455c35d3a8bda7d31059c6
mahd1ar <32998122+mahdi-yar@users.noreply.github.com>
2022-01-22 19:57:40 +0330

    first commi

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'iransans': ['iransans'],
+      'vazir': ['Vazir'],
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+}
