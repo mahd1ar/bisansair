35743f9017f59b1371567c5efceabc0688f0dcc0
mahd1ar <32998122+mahdi-yar@users.noreply.github.com>
2022-01-25 22:34:03 +0330

    responsive 50%

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,24 @@ module.exports = {
       'iransans': ['iransans'],
       'vazir': ['Vazir'],
     },
+    // container: {
+    //   backgroundColor: "red",
+    //   maxWidth: {
+    //     'xl': '1040px',
+    //     '2xl': '1040px'
+    //   }
+    // },
+    // container: {
+    //   padding: '2rem',
+    // },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      }
+      },
+      screens: {
+        'xl': '1040px',
+        '2xl': '1040px',
+      },
     },
   },
   plugins: [],
