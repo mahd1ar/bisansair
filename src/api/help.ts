8ebd677a44e4c7ef75d4cbfb5b751cd234f742d5
mahd1ar <32998122+mahdi-yar@users.noreply.github.com>
2022-02-08 10:49:24 +0330

    site manifest

---
+++ src/api/help.ts
@@ -0,0 +1,51 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import fs from "fs";
+
+
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+    const head = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HELP</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    </head>
+    
+    <body>
+        <div class="container">
+            <div class="row">
+    `
+    
+    const tail = `</div></div></body></html>`
+    
+    
+    const list =  fs.readdirSync("..");
+    
+    const cards = list.filter(i => i.split(".")[0] === "html").map(i=>{
+
+        return `<div class="col s12 m6">
+                    <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">Card Title</span>
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                                I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#">${i}</a>
+                            <a href="#">This is a link</a>
+                        </div>
+                    </div>
+                </div>`
+
+    })
+
+    const template = head + cards + tail;
+
+    response.status(200).send(template)
+        
+}
